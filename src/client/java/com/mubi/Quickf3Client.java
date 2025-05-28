660d2d4c81fe6f2f837393026fb9de0a5d152d54
mubduster <mubassirmuneeb51@gmail.com>
2025-05-29 02:26:26 +0500

    initial commit

---
+++ src/client/java/com/mubi/Quickf3Client.java
@@ -0,0 +1,10 @@
+package com.mubi;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Quickf3Client implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
