97db1b840f89a8aac32fbfebde2036f86f668584
mubduster <mubassirmuneeb51@gmail.com>
2025-06-04 00:48:53 +0500

    made some extra options

--- src/client/java/com/mubi/Quickf3Client.java
+++ src/client/java/com/mubi/Quickf3Client.java
@@ -22,10 +22,13 @@ public class Quickf3Client implements ClientModInitializer {
 
     private static KeyBinding keyBinding; // initializing variable for setting key bind F4
     private static KeyBinding keyBindingF3; // initializing variable for setting key bind F3
+    private static KeyBinding keyBindingmode; // initializing the variable for setting the key for modes
     
     private static boolean showCords = true; // variable to toggle the overlay
     
     private static boolean f3condition = true; // variable to make sure that the overlay doesnt overlap with the F3 menu
+
+    private static int mode = 1; 
 
     private static void render(DrawContext context, RenderTickCounter tickCounter) {
     
@@ -40,9 +43,23 @@ public class Quickf3Client implements ClientModInitializer {
                 else {             // to toggle on the overlay when the F3 menu is closed
                     showCords = true;
                     f3condition = true;
+                    
+                    if (keyBindingmode.wasPressed()) {
+                        mode+=1;
+                            if (mode > 3){
+                                mode=1;
+                            }
+                    }
                 }
             }
-            
+            else if (showCords == true){
+                if (keyBindingmode.wasPressed()){
+                    mode+=1;
+                    if (mode > 3){
+                        mode=1;
+                    }
+                }
+            }            
 
             if (showCords){ // rendering code for the overlay if overlay toggled
                 TextRenderer textRenderer= MinecraftClient.getInstance().textRenderer; // variable for initializing text rendering
@@ -52,17 +69,38 @@ public class Quickf3Client implements ClientModInitializer {
                 double z = player.getZ(); // z coordinate 64 bit float
 
                 int fps = MinecraftClient.getInstance().getCurrentFps(); // initialize FPS variable
+                if (mode == 2){
+                    context.fill( 0, 0, 50, 17, 0 ,0x77333333); // This renders the background. Remember x and y graph is flipped for some reason. (x1, x2, y1, y2, z(which layer is this on), colour).
+                    context.drawBorder( 0, 0, 50, 17, 0x88444444); // This renders the outline for background.These lengths corrispond to the lenghts of fill, and x, y are is position. (x, y, weidth, height, colour)
+                }
+                else if (mode == 3){
+                    context.fill( 0, 0, 110, 17, 0 ,0x77333333); // This renders the background. Remember x and y graph is flipped for some reason. (x1, x2, y1, y2, z(which layer is this on), colour).
+                    context.drawBorder( 0, 0, 110, 17, 0x88444444); // This renders the outline for background.These lengths corrispond to the lenghts of fill, and x, y are is position. (x, y, weidth, height, colour)
+                }
+                else if (mode == 1){
+                    context.fill( 0, 0, 165, 17, 0 ,0x77333333); // This renders the background. Remember x and y graph is flipped for some reason. (x1, x2, y1, y2, z(which layer is this on), colour).
+                    context.drawBorder( 0, 0, 165, 17, 0x88444444); // This renders the outline for background.These lengths corrispond to the lenghts of fill, and x, y are is position. (x, y, weidth, height, colour)
+                }
                 
-                context.fill( 0, 0, 145, 17, 0 ,0x77333333); // This renders the background. Remember x and y graph is flipped for some reason. (x1, x2, y1, y2, z(which layer is this on), colour).
-                context.drawBorder( 0, 0, 145, 17, 0x88444444); // This renders the outline for background.These lengths corrispond to the lenghts of fill, and x, y are is position. (x, y, weidth, height, colour)
+                if (mode == 1){
+                    String coords = String.format("X: %.0f, Y: %.0f, Z: %.0f", x, y, z); // coordinates render format
 
-                String coords = String.format("X: %.0f, Y: %.0f, Z: %.0f", x, y, z); // coordinates render format
-            
-                context.drawText(textRenderer, coords, 50, 5, 0x00FFFFFF, true); // renders coordinates
+                    context.drawText(textRenderer, coords, 50, 5, 0x00FFFFFF, true); // renders coordinates
 
-                String FPS = String.format("FPS: "+fps); // FPS render format
+                    String FPS = String.format("FPS: "+fps); // FPS render format
 
-                context.drawText(textRenderer, FPS, 3, 5, 0x00FFFFFF, true); // renders FPS
+                    context.drawText(textRenderer, FPS, 3, 5, 0x00FFFFFF, true); // renders FPS
+                }
+                else if (mode == 2){
+                    String FPS = String.format("FPS: "+fps); // FPS render format
+
+                    context.drawText(textRenderer, FPS, 3, 5, 0x00FFFFFF, true); // renders FPS
+                }
+                else if (mode == 3 ){
+                    String coords = String.format("X: %.0f, Y: %.0f, Z: %.0f", x, y, z); // coordinates render format
+
+                    context.drawText(textRenderer, coords, 3, 5, 0x00FFFFFF, true); // renders coordinates
+                }
             }
             
         }
@@ -85,6 +123,13 @@ public class Quickf3Client implements ClientModInitializer {
             InputUtil.Type.KEYSYM,
             GLFW.GLFW_KEY_F3,
             "category.Quickf3.unshow"
+        ));
+
+        keyBindingmode = KeyBindingHelper.registerKeyBinding(new KeyBinding(  //initialize keybind left Alt
+            "key.Quickf3.toggled",
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_LEFT_ALT,
+            "category.Quickf3.mode"
         ));
     }
 }
