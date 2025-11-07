f3d5a7e58cd3b2fe5b941dd50fa536a1c08f5ded
mubduster <mubassirmuneeb51@gmail.com>
2025-11-07 23:12:37 +0500

    Updated the Mod to support minecraft fabric versions 1.21.9 and 1.21.10.

--- src/main/java/com/mubi/QuickF3.java
+++ src/main/java/com/mubi/QuickF3.java
@@ -15,10 +15,11 @@ public class QuickF3 implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Quick F3 ready!\n Boo!");
 	}
 }
